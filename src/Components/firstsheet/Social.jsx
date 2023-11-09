4f2ce6ef5fb3c8cf2c6ef0ac8b96a7ec8f3c0f26
MUHAMMED-SHAIKH-ZAHID <muhdshaikhzahid@gmail.com>
2023-11-09 11:15:14 +0530

    linkedin link changed

--- src/Components/firstsheet/Social.jsx
+++ src/Components/firstsheet/Social.jsx
@@ -7,7 +7,7 @@ const Social = () => {
   return (
     <div className='grid grid-cols-[max-content] gap-y-5 '>
          <a href="https://www.instagram.com/_shaikh_zahid/" target='_blank' rel="noopener noreferrer" ><RxInstagramLogo className='h-5 w-5 hover:iconzoom'  /></a>  
-          <a href='https://github.com/MUHAMMED-SHAIKH-ZAHID' target='_blank' rel="noopener noreferrer"  > <CiLinkedin className='h-6 w-5 hover:iconzoom' /></a>  
+          <a href='https://www.linkedin.com/in/muhdshaikhzahid/' target='_blank' rel="noopener noreferrer"  > <CiLinkedin className='h-6 w-5 hover:iconzoom' /></a>  
           <a href='https://github.com/MUHAMMED-SHAIKH-ZAHID' target='_blank' rel="noopener noreferrer"  > <VscGithubAlt className='h-5 w-5 hover:iconzoom' /></a>  
     </div>
   )
